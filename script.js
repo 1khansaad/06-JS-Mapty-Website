58dd07d263fb127933bef91fdcf0df607c916a31
1khansaad <saadkhan8268@gmail.com>
2022-08-16 22:03:50 +0530

    added leafler js library and implemented some functionality

--- script.js
+++ script.js
@@ -11,8 +11,70 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(function(){
-    console.log(position)
-}, function(){
-    console.log('could not get your position')
-})
+let map, mapEvent;
+class App{
+    #map;
+    #mapEvent;
+    constructor(){
+        this._getPosition()
+        form.addEventListener('submit', this._newWorkout.bind(this))     
+        inputType.addEventListener('change', this._toggleElevationField)
+    }
+    _getPosition(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this) , function(){
+                console.log('could not get your position')
+            })   
+        }
+    }
+
+    _loadMap(position){
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`)
+    
+            const coordsArr = [latitude, longitude]
+    
+           this.#map = L.map('map').setView(coordsArr, 13);
+    
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(this.#map);
+    
+            L.marker(coordsArr).addTo(this.#map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+
+            this.#map.on('click', this._showForm.bind(this))
+    }
+    _showForm(mapE){
+            this.#mapEvent = mapE
+            form.classList.remove('hidden');
+            inputDistance.focus()
+    }
+    _toggleElevationField(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    }
+    _newWorkout(e){
+        e.preventDefault();
+        // clearing field
+        inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+                
+        const { lat, lng } = this.#mapEvent.latlng;
+            L.marker([lat, lng]).addTo(this.#map)
+            .bindPopup(
+                 L.popup({
+                 maxWidth: 250,
+                 minWidth: 100,
+                 autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+        )
+        .setPopupContent('workout always help')
+        .openPopup();
+    }
+}
+    
+const app = new App();
